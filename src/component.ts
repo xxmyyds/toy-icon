1237f5e0130ad3cfeeb4adb64f1f305fd56bc034
Code_xxmyyds <xxmyyds@163.com>
2023-01-18 19:04:11 +0800

    feat(iocn): add icons

--- src/component.ts
+++ src/component.ts
@@ -1,4 +1,5 @@
 export { default as ToyIconAdd } from './svg/toy-add.svg'
-export { default as ToyIconIosPlus } from './svg/toy-ios-plus.svg'
-export { default as ToyIconIosMinus } from './svg/toy-ios-minus.svg'
+export { default as ToyIconAlbums } from './svg/toy-albums.svg'
+export { default as ToyIconFavorite } from './svg/toy-favorite.svg'
+export { default as ToyIconIosRemove } from './svg/toy-ios-remove.svg'
 export { default as ToyIconLoading } from './svg/toy-loading.svg'
